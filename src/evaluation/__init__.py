dddd137f2656460de0a4abcb2e195c3a85459df2
leejaeyoung-cpu <brookinljy@gmail.com>
2026-04-05 12:28:09 +0900

    fix: lazy import model_evaluator to avoid seaborn dep in CI

--- src/evaluation/__init__.py
+++ src/evaluation/__init__.py
@@ -1,10 +1,17 @@
 """Evaluation package"""
 
 from .data_validator import DataQualityValidator
-from .model_evaluator import ModelEvaluator, BiologicalValidator
+
+try:
+    from .model_evaluator import ModelEvaluator, BiologicalValidator
+    _has_model_evaluator = True
+except ImportError:
+    _has_model_evaluator = False
+    ModelEvaluator = None
+    BiologicalValidator = None
 
 __all__ = [
-    'DataQualityValidator',
-    'ModelEvaluator',
-    'BiologicalValidator'
+    "DataQualityValidator",
+    "ModelEvaluator",
+    "BiologicalValidator",
 ]
